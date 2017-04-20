6389a0275402152ea6fb7a6e63aefcb8470266e3
Rafael Arcanjo <rafaelarcanjo@gmail.com>
2017-04-19 23:05:37 -0300

    Finalização do Exercício

--- testeIO/src/org/arcanjo/alura/java3/testeIO/Exercicio1.java
+++ testeIO/src/org/arcanjo/alura/java3/testeIO/Exercicio1.java
@@ -1,10 +1,27 @@
 package org.arcanjo.alura.java3.testeIO;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class Exercicio1 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 			
+		InputStream is = System.in; //new FileInputStream("arquivo.txt");
+		InputStreamReader isr = new InputStreamReader(is);
+		BufferedReader br = new BufferedReader(isr);
 		
+		System.out.println("Digite seu texto: ");
+		String linha = br.readLine();
+		
+		while (linha != null){
+			
+			System.out.println(linha);
+			linha = br.readLine();
+			
+		}
 		
 	}
 
