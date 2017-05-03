23ba33092763c3b4dcd8955b0376e508c6bbd485
Rafael Arcanjo <rafaelarcanjo@gmail.com>
2017-05-02 23:20:40 -0300

    Exercicios

--- testeIO/src/org/arcanjo/alura/java3/testeIO/EntradaScanner.java
+++ testeIO/src/org/arcanjo/alura/java3/testeIO/EntradaScanner.java
@@ -1,27 +1,35 @@
 package org.arcanjo.alura.java3.testeIO;
 
+import java.io.BufferedWriter;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.util.Scanner;
 
 public class EntradaScanner {
 
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) throws IOException {
 
 		InputStream is = new FileInputStream("arquivo.txt");
 		Scanner entrada = new Scanner(is);
 		
-		System.out.println("Digite sua mensagem: ");
+        OutputStream os = new FileOutputStream("saida.txt");
+        OutputStreamWriter osw = new OutputStreamWriter(os);
+        BufferedWriter bw = new BufferedWriter(osw);
 		
 		while (entrada.hasNextLine()){
 			
-			System.out.println(entrada.nextLine());
+			String linha = entrada.nextLine();
+			bw.write(linha);
+			bw.newLine();
 			
 		}
 		
 		entrada.close();
-		
+		bw.close();
 	}
 
 }
