99bbce6be016c8d9fdd61f2a3bfcf338a4f1e46a
Rafael Arcanjo <rafaelarcanjo@gmail.com>
2017-05-05 14:39:45 -0300

    Update

--- testeIO/src/org/arcanjo/alura/java3/testeIO/EntradaScanner.java
+++ testeIO/src/org/arcanjo/alura/java3/testeIO/EntradaScanner.java
@@ -1,6 +1,5 @@
 package org.arcanjo.alura.java3.testeIO;
 
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -12,11 +11,13 @@ public class EntradaScanner {
 
 	public static void main(String[] args) throws IOException {
 
-		InputStream is = new FileInputStream("arquivo.txt");
+		InputStream is = System.in;
 		Scanner entrada = new Scanner(is);
 		
         OutputStream os = new FileOutputStream("saida.txt");
         PrintStream saida = new PrintStream(os);
+        
+        System.out.println("Digite sua mensagem: ");
 		
 		while (entrada.hasNextLine()){
 			
